b8308bc2c08bc588c7f33755cc58785101e39c93
Marcus <145341135+mlundberg93@users.noreply.github.com>
2024-03-05 13:36:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LIA_Uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
