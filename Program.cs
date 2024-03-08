e29dfb8e9af70372900173f349c201c4a2cb4883
Marcus <145341135+mlundberg93@users.noreply.github.com>
2024-03-08 13:08:08 +0100

    Lagt till kommentarer

--- Program.cs
+++ Program.cs
@@ -6,15 +6,19 @@ namespace LIA_Uppgift
     {
         static void Main(string[] args)
         {
+            //XML-fil läggs i följande mapstruktur: ..\..\Source\Repos\MyProjects\LIA_Uppgift\bin\Debug\net8.0\sma.gentext.xml
             string fileName = "sma_gentext.xml";
             string docPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
 
+            //Läser in XML-dokumentet
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load(docPath);
 
+            //Sökning efter element
             XmlNode search = xmlDoc.SelectSingleNode("//trans-unit[@id='42007']/target");
             string value = search.InnerText;
 
+            //Skapar och skriver value till ny fil
             string createFile = "XML-Data.txt";
             File.WriteAllText(createFile, value);
 
