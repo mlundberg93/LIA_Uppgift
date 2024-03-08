6907981b8a40070193bf61f10b03cc01ff970c2d
Marcus <145341135+mlundberg93@users.noreply.github.com>
2024-03-08 11:08:31 +0100

    Struktur för att läsa från XML fil skapad

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LIA_Uppgift
+﻿using System.Text;
+using System.Xml;
+
+namespace LIA_Uppgift
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string fileName = "sma_gentext.xml";
+            string docPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(docPath);
+
         }
     }
 }
